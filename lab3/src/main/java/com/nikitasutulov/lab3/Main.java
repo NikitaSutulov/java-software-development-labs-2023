ea872c3882002b51c272f1f53a094b3f0c16ee6f
Nikita Sutulov <notsid212@gmail.com>
2023-10-27 13:53:11 +0300

    lab 3 task done

---
+++ lab3/src/main/java/com/nikitasutulov/lab3/Main.java
@@ -0,0 +1,53 @@
+package com.nikitasutulov.lab3;
+
+import java.io.IOException;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        try {
+            String[] words = readWordsFromConsole();
+            String[] filteredWords = filterUniqueWords(words);
+            String[] sortedUniqueWords = filteredWords.clone();
+            Arrays.sort(sortedUniqueWords, Comparator.comparing(String::toLowerCase));
+            System.out.println("\nUnfiltered words");
+            System.out.println(arrayToString(words));
+            System.out.println("\nUnique words");
+            System.out.println(arrayToString(filteredWords));
+            System.out.println("\nUnique sorted words");
+            System.out.println(arrayToString(sortedUniqueWords));
+        } catch (IOException e) {
+            throw new RuntimeException("There was a problem while reading the text from the console input: " + e.getMessage());
+        }
+    }
+
+    public static String[] readWordsFromConsole() throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String[] words = line.split("[,.!?:\\- ]+");
+        for (String word : words) {
+            if (!word.matches("[a-zA-Zа-яА-Яґєїі'`]+")) {
+                scanner.close();
+                throw new IOException("The words can only contain letters, but there is word which does not: " + word);
+            }
+        }
+        scanner.close();
+        return words;
+    }
+
+    public static String[] filterUniqueWords(String[] unfilteredWords) {
+        Map<String, String> uniqueWordsMap = new LinkedHashMap<>();
+
+        for (String word : unfilteredWords) {
+            String lowercaseWord = word.toLowerCase();
+            uniqueWordsMap.putIfAbsent(lowercaseWord, word);
+        }
+
+        return uniqueWordsMap.values().toArray(new String[0]);
+    }
+
+    public static String arrayToString(String[] array) {
+        return Arrays.toString(array).replaceAll("[\\[\\]]", "");
+    }
+
+}
