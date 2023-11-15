04f257e089c02a0a524b2992931536665ee8e09a
Nikita Sutulov <notsid212@gmail.com>
2023-11-15 13:33:03 +0200

    lab 5 made LinguisticExpression class abstract
    Made the laboratory work 5 class LinguisticExpression abstract, as it must not have any instances itself.

--- lab5/src/main/java/com/nikitasutulov/lab5/entities/LinguisticExpression.java
+++ lab5/src/main/java/com/nikitasutulov/lab5/entities/LinguisticExpression.java
@@ -3,5 +3,5 @@ package com.nikitasutulov.lab5.entities;
 /**
  * The LinguisticExpression class is a base class for linguistic expressions.
  */
-public class LinguisticExpression {
+public abstract class LinguisticExpression {
 }
